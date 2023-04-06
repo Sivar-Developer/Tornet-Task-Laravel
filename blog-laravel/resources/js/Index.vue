c8075216ab35f6fc541048397c458930c2ed4071
Sivar-Developer <sarkawtsivar@gmail.com>
2023-04-06 22:05:49 +0300

    Login and register vue validaiton

--- blog-laravel/resources/js/Index.vue
+++ blog-laravel/resources/js/Index.vue
@@ -4,23 +4,57 @@
             <div class="col-6 offset-3">
                 <div class="card">
                     <div class="card-body">
-                        <form action="#" @submit.prevent="handleLogin">
+                        <!-- Login Screen -->
+                        <form v-if="screen == 'login'" action="#" @submit.prevent="handleLogin">
                             <h3>Login form</h3>
                             <div class="form-row">
-                                <input type="email" name="email" v-model="formData.email" class="form-control"
-                                    :class="{ 'is-invalid': authErrors?.errors?.email }" placeholder="Email Address">
-                                <div class="invalid-feedback" v-for="msg in authErrors?.errors?.email" v-text="msg"></div>
+                                <input type="email" name="email" v-model="loginFormData.email" class="form-control"
+                                    :class="{ 'is-invalid': loginAuthErrors?.errors?.email }" placeholder="Email Address">
+                                <div class="invalid-feedback" v-for="msg in loginAuthErrors?.errors?.email" v-text="msg"></div>
                             </div>
                             <div class="form-row">
-                                <input type="password" name="password" v-model="formData.password"
-                                    :class="{ 'is-invalid': authErrors?.errors?.password }" class="form-control"
+                                <input type="password" name="password" v-model="loginFormData.password"
+                                    :class="{ 'is-invalid': loginAuthErrors?.errors?.password }" class="form-control"
                                     placeholder="Password">
-                                <div class="invalid-feedback" v-for="msg in authErrors?.errors?.password" v-text="msg">
+                                <div class="invalid-feedback" v-for="msg in loginAuthErrors?.errors?.password" v-text="msg">
                                 </div>
                             </div>
                             <div class="form-row">
                                 <button type="submit" class="btn btn-primary">Login</button>
-                                <button type="button" class="btn btn-link">Register?</button>
+                                <button type="button" class="btn btn-link" v-on:click="screen = 'register'">Register?</button>
+                            </div>
+                        </form>
+
+                        <!-- Register Screen -->
+                        <form v-if="screen == 'register'" action="#" @submit.prevent="handleRegister">
+                            <h3>Login form</h3>
+                            <div class="form-row">
+                                <input type="text" name="name" v-model="registerFormData.name" class="form-control"
+                                    :class="{ 'is-invalid': registerAuthErrors?.errors?.name }" placeholder="Full Name">
+                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.name" v-text="msg"></div>
+                            </div>
+                            <div class="form-row">
+                                <input type="email" name="email" v-model="registerFormData.email" class="form-control"
+                                    :class="{ 'is-invalid': registerAuthErrors?.errors?.email }" placeholder="Email Address">
+                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.email" v-text="msg"></div>
+                            </div>
+                            <div class="form-row">
+                                <input type="password" name="password" v-model="registerFormData.password"
+                                    :class="{ 'is-invalid': registerAuthErrors?.errors?.password }" class="form-control"
+                                    placeholder="Password">
+                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.password" v-text="msg">
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <input type="password" name="password_confirmation" v-model="registerFormData.password_confirmation"
+                                    :class="{ 'is-invalid': registerAuthErrors?.errors?.password_confirmation }" class="form-control"
+                                    placeholder="Password Confirmation">
+                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.password_confirmation" v-text="msg">
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <button type="submit" class="btn btn-primary">Register</button>
+                                <button type="button" class="btn btn-link" v-on:click="screen = 'login'">login?</button>
                             </div>
                         </form>
                     </div>
@@ -28,8 +62,9 @@
             </div>
         </div>
 
+        <!-- Posts Screen -->
         <div class="row mt-4" v-if="loggedIn">
-            <div class="col-6 offset-3">
+            <div class="col-8">
                 <h3>My Posts</h3>
                 <div class="btn-group float-right" role="group" aria-label="Basic outlined example">
                     <button type="button" class="btn btn-outline-primary" v-on:click="language = 'en'">English</button>
@@ -43,6 +78,9 @@
                     <strong v-if="language == 'ku'" v-text="post?.title?.ku"></strong>
                 </div>
                 <Bootstrap5Pagination :data="posts" @pagination-change-page="getPosts" />
+            </div>
+            <div class="col-4">
+                <h3>Categories</h3>
             </div>
         </div>
 
@@ -59,19 +97,27 @@ export default {
         return {
             language: 'en',
             loggedIn: false,
+            screen: 'login',
             headers: {},
             posts: {},
-            authErrors: {},
-            formData: {
+            loginAuthErrors: {},
+            registerAuthErrors: {},
+            loginFormData: {
                 email: '',
                 password: '',
-            }
+            },
+            registerFormData: {
+                name: '',
+                email: '',
+                password: '',
+                password_confirmation: '',
+            },
         }
     },
     methods: {
         handleLogin() {
             axios.get('/sanctum/csrf-cookie').then((response) => {
-                axios.post('/api/login', this.formData).then((response) => {
+                axios.post('/api/login', this.loginFormData).then((response) => {
                     this.loggedIn = true
                     const config = {
                         headers: {
@@ -82,7 +128,25 @@ export default {
                     this.headers = config
                     this.getPosts()
                 }, (errors) => {
-                    this.authErrors = errors.response?.data
+                    this.loginAuthErrors = errors.response?.data
+                    this.loggedIn = false
+                })
+            })
+        },
+        handleRegister() {
+            axios.get('/sanctum/csrf-cookie').then((response) => {
+                axios.post('/api/register', this.registerFormData).then((response) => {
+                    this.loggedIn = true
+                    const config = {
+                        headers: {
+                            'Accept': 'application/json',
+                            'Authorization': `Bearer ${response.data?.token}`
+                        }
+                    }
+                    this.headers = config
+                    this.getPosts()
+                }, (errors) => {
+                    this.registerAuthErrors = errors.response?.data
                     this.loggedIn = false
                 })
             })
