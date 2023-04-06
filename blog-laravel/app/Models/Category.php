a807d31d71666b155b3ba284a2396301ee7e2ba6
Sivar-Developer <sarkawtsivar@gmail.com>
2023-04-06 14:28:34 +0300

    Setup translatable for models

--- blog-laravel/app/Models/Category.php
+++ blog-laravel/app/Models/Category.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Category extends Model
 {
     use HasFactory;
+    use HasTranslations;
 
     protected $guarded = [];
+
+    public $translatable = ['title'];
 }
--- blog-laravel/app/Models/Post.php
+++ blog-laravel/app/Models/Post.php
@@ -5,11 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Translatable\HasTranslations;
 
 class Post extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use HasTranslations;
 
     protected $guarded = [];
+
+    public $translatable = ['title'];
 }
