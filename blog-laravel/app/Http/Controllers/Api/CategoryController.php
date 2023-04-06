a313c0c3fd7ff2ed76942c569c7fd2b232df1f76
Sivar-Developer <sarkawtsivar@gmail.com>
2023-04-06 14:27:09 +0300

    Category requests

--- blog-laravel/app/Http/Controllers/Api/CategoryController.php
+++ blog-laravel/app/Http/Controllers/Api/CategoryController.php
@@ -5,13 +5,50 @@ namespace App\Http\Controllers\Api;
 use App\Models\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
 
 class CategoryController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         $cateogries = Category::latest()->get();
 
         return response()->json($categories, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryStoreRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryUpdateRequest $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
     }
 }
