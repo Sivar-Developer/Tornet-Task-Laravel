d9edf53f039ab7f49a025d58e894dd9ae67b8c3d
Sivar-Developer <sarkawtsivar@gmail.com>
2023-04-06 14:02:14 +0300

    Categories migration table with posts relationship

--- blog-laravel/database/migrations/2023_04_06_105524_create_categories_table.php
+++ blog-laravel/database/migrations/2023_04_06_105524_create_categories_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->text('title');
+            $table->text('image');
+        });
+
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreignId('category_id')->nullable()
+                ->constrained('categories')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
         });
     }
 
@@ -22,6 +30,10 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
+        });
         Schema::dropIfExists('categories');
     }
 };
