880c2772ce3a9cfd805be5d8a12c6940abe0be1f
Sivar-Developer <sarkawtsivar@gmail.com>
2023-04-06 22:43:39 +0300

    Vue get categories list

--- blog-laravel/app/Http/Controllers/Api/CategoryController.php
+++ blog-laravel/app/Http/Controllers/Api/CategoryController.php
@@ -20,7 +20,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::all();
+        $categories = Category::withCount('posts')->get();
 
         return response()->json($categories, 200);
     }
--- blog-laravel/resources/js/Index.vue
+++ blog-laravel/resources/js/Index.vue
@@ -10,7 +10,8 @@
                             <div class="form-row">
                                 <input type="email" name="email" v-model="loginFormData.email" class="form-control"
                                     :class="{ 'is-invalid': loginAuthErrors?.errors?.email }" placeholder="Email Address">
-                                <div class="invalid-feedback" v-for="msg in loginAuthErrors?.errors?.email" v-text="msg"></div>
+                                <div class="invalid-feedback" v-for="msg in loginAuthErrors?.errors?.email" v-text="msg">
+                                </div>
                             </div>
                             <div class="form-row">
                                 <input type="password" name="password" v-model="loginFormData.password"
@@ -21,7 +22,8 @@
                             </div>
                             <div class="form-row">
                                 <button type="submit" class="btn btn-primary">Login</button>
-                                <button type="button" class="btn btn-link" v-on:click="screen = 'register'">Register?</button>
+                                <button type="button" class="btn btn-link"
+                                    v-on:click="screen = 'register'">Register?</button>
                             </div>
                         </form>
 
@@ -31,25 +33,31 @@
                             <div class="form-row">
                                 <input type="text" name="name" v-model="registerFormData.name" class="form-control"
                                     :class="{ 'is-invalid': registerAuthErrors?.errors?.name }" placeholder="Full Name">
-                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.name" v-text="msg"></div>
+                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.name" v-text="msg">
+                                </div>
                             </div>
                             <div class="form-row">
                                 <input type="email" name="email" v-model="registerFormData.email" class="form-control"
-                                    :class="{ 'is-invalid': registerAuthErrors?.errors?.email }" placeholder="Email Address">
-                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.email" v-text="msg"></div>
+                                    :class="{ 'is-invalid': registerAuthErrors?.errors?.email }"
+                                    placeholder="Email Address">
+                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.email" v-text="msg">
+                                </div>
                             </div>
                             <div class="form-row">
                                 <input type="password" name="password" v-model="registerFormData.password"
                                     :class="{ 'is-invalid': registerAuthErrors?.errors?.password }" class="form-control"
                                     placeholder="Password">
-                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.password" v-text="msg">
+                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.password"
+                                    v-text="msg">
                                 </div>
                             </div>
                             <div class="form-row">
-                                <input type="password" name="password_confirmation" v-model="registerFormData.password_confirmation"
-                                    :class="{ 'is-invalid': registerAuthErrors?.errors?.password_confirmation }" class="form-control"
-                                    placeholder="Password Confirmation">
-                                <div class="invalid-feedback" v-for="msg in registerAuthErrors?.errors?.password_confirmation" v-text="msg">
+                                <input type="password" name="password_confirmation"
+                                    v-model="registerFormData.password_confirmation"
+                                    :class="{ 'is-invalid': registerAuthErrors?.errors?.password_confirmation }"
+                                    class="form-control" placeholder="Password Confirmation">
+                                <div class="invalid-feedback"
+                                    v-for="msg in registerAuthErrors?.errors?.password_confirmation" v-text="msg">
                                 </div>
                             </div>
                             <div class="form-row">
@@ -64,23 +72,61 @@
 
         <!-- Posts Screen -->
         <div class="row mt-4" v-if="loggedIn">
-            <div class="col-8">
-                <h3>My Posts</h3>
-                <div class="btn-group float-right" role="group" aria-label="Basic outlined example">
+            <div class="col-10 offset-1 mb-4">
+                <div class="btn-group" role="group" aria-label="Basic outlined example">
                     <button type="button" class="btn btn-outline-primary" v-on:click="language = 'en'">English</button>
                     <button type="button" class="btn btn-outline-primary" v-on:click="language = 'ar'">العربية</button>
                     <button type="button" class="btn btn-outline-primary" v-on:click="language = 'ku'">کوردی</button>
                 </div>
+                <button class="btn btn-dark float-end">Logout</button>
+            </div>
+            <br>
+            <div class="col-6 offset-1">
+                <h3 class="mb-4">My Posts</h3>
                 <div class="post" v-for="(post, index) in posts?.data" :key="post.id">
-                    <em v-text="post?.created_at"></em><br>
-                    <strong v-if="language == 'en'" v-text="post?.title?.en"></strong>
-                    <strong v-if="language == 'ar'" v-text="post?.title?.ar"></strong>
-                    <strong v-if="language == 'ku'" v-text="post?.title?.ku"></strong>
-                </div>
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title" v-if="language == 'en'" v-text="post?.title?.en"></h5>
+                            <h5 class="card-title" v-if="language == 'ar'" v-text="post?.title?.ar"></h5>
+                            <h5 class="card-title" v-if="language == 'ku'" v-text="post?.title?.ku"></h5>
+                            <p class="card-text" v-text="post?.created_at"></p>
+                            <div class="btn-group me-2" role="group" aria-label="First group">
+                                <button type="button" class="btn btn-light">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path
+                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fill-rule="evenodd"
+                                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                    </svg>
+                                </button>
+                                <button type="button" class="btn btn-light">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-trash" viewBox="0 0 16 16">
+                                        <path
+                                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                                        <path
+                                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div><br>
+                <div class="mb-2"></div>
                 <Bootstrap5Pagination :data="posts" @pagination-change-page="getPosts" />
             </div>
             <div class="col-4">
                 <h3>Categories</h3>
+                <ul class="list-group mt-4" v-if="categories.length">
+                    <li class="list-group-item d-flex justify-content-between align-items-center"
+                        v-for="category in categories" :key="category.id">
+                        <div v-if="language == 'en'" v-text="category?.title?.en"></div>
+                        <div v-if="language == 'ar'" v-text="category?.title?.ar"></div>
+                        <div v-if="language == 'ku'" v-text="category?.title?.ku"></div>
+                        <span class="badge bg-primary rounded-pill" v-text="category?.posts_count"></span>
+                    </li>
+                </ul>
             </div>
         </div>
 
@@ -99,7 +145,6 @@ export default {
             loggedIn: false,
             screen: 'login',
             headers: {},
-            posts: {},
             loginAuthErrors: {},
             registerAuthErrors: {},
             loginFormData: {
@@ -112,6 +157,8 @@ export default {
                 password: '',
                 password_confirmation: '',
             },
+            posts: {},
+            categories: []
         }
     },
     methods: {
@@ -126,6 +173,7 @@ export default {
                         }
                     }
                     this.headers = config
+                    this.getCategories()
                     this.getPosts()
                 }, (errors) => {
                     this.loginAuthErrors = errors.response?.data
@@ -144,6 +192,7 @@ export default {
                         }
                     }
                     this.headers = config
+                    this.getCategories()
                     this.getPosts()
                 }, (errors) => {
                     this.registerAuthErrors = errors.response?.data
@@ -157,12 +206,17 @@ export default {
                 console.log(this.posts)
             }, () => this.loggedIn = false)
         },
+        getCategories() {
+            axios.get('/api/categories', this.headers).then((response) => {
+                console.log(response.data)
+                this.categories = response.data
+                console.log(this.categories)
+            }, () => this.loggedIn = false)
+        },
     },
 }
 </script>
 
-<style>
-.form-row {
+<style>.form-row {
     margin-bottom: 8px;
-}
-</style>
+}</style>
